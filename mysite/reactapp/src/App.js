8a504b9e01af6950066b0d6dcbb8b9a6538e6f78
debaghosh <debanjanaghosh99@gmail.com>
2020-09-28 15:14:43 +0530

    first commit

---
+++ mysite/reactapp/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+      <h3>This is my first Django+React Application.</h3>
+    </div>
+  );
+}
+
+export default App;
